851bacff275ee6de9c2a1abd946d0b97e9fd19bc
jkrick <jkrick@caltech.edu>
2023-05-12 09:00:15 -0700

    Update light_curves/code/HCV_functions.py
    Co-authored-by: Troy Raen <raen@ipac.caltech.edu>

--- light_curves/code/HCV_functions.py
+++ light_curves/code/HCV_functions.py
@@ -223,7 +223,7 @@ def HCV_get_lightcurves(coords_list, labels_list, radius):
 
     Returns
     -------
-    df_lc : pandas MultiIndexDFObject
+    df_lc : MultiIndexDFObject
         the main data structure to store all light curves
     """
 
