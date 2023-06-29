0ae5d80ed5016b6ff83a3eddbf1b4865d5bd581a
Troy Raen <raen@ipac.caltech.edu>
2023-06-29 16:56:49 -0700

    reinstate original code

--- light_curves/code/ztf_functions.py
+++ light_curves/code/ztf_functions.py
@@ -1,172 +1,123 @@
-import re
-
-import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import pyvo
+import requests
+from alerce.core import Alerce
+from astropy.coordinates import SkyCoord
+from astropy.io import ascii
+from tqdm import tqdm
+
 from data_structures import MultiIndexDFObject
 
 
-DATARELEASE = "dr17"
-URLBASE = f"https://irsa.ipac.caltech.edu/data/ZTF/lc/lc_{DATARELEASE}"
-# get a list of files in the dataset using the checksums file
-DSFILES = (
-    pd.read_table(f"{URLBASE}/checksum.md5", sep="\s+", names=["md5", "path"], usecols=["path"])
-    .squeeze()
-    .str.removeprefix("./")
-    .to_list()
-)
+def ZTF_id2coord(object_ids,coords,labels,verbose=1):
+    """ To find and append coordinates of objects with only ZTF obj name
+      
+    Parameters
+    ----------
+    object_ids: list of strings
+        eg., [ "ZTF18accqogs", "ZTF19aakyhxi", "ZTF19abyylzv", "ZTF19acyfpno"]
+    coords : list of astropy skycoords
+        the coordinates of the targets for which a user wants light curves
+    labels: list of strings
+        journal articles associated with the target coordinates
+    verbose: int
+        print out debugging info (1) or not(0)
+    """
+    
+    
+    
+    alerce = Alerce()
+    objects = alerce.query_objects(oid=object_ids, format="pandas")
+    ztf_coords = [SkyCoord(ra, dec, frame='icrs', unit='deg') for ra, dec in zip(objects['meanra'],objects['meandec'])]
+    ztf_labels = ['ZTF-Objname' for ra in objects['meanra']]
+    coords.extend(ztf_coords)
+    labels.extend(ztf_labels)
+    if verbose:
+        print('number of ztf coords added by Objectname:',len(objects['meanra']))
+    
 
-
-def ZTF_get_lightcurve(coords_list, labels_list, ztf_radius=0.000278 * u.deg):
-    """Function to add the ZTF lightcurves in all three bands to a multiframe data structure
-
+def ZTF_get_lightcurve(coords_list, labels_list, plotprint=1, ztf_radius=0.000278):
+    """ Function to add the ZTF lightcurves in all three bands to a multiframe data structure 
+     
     Parameters
     ----------
     coords_list : list of astropy skycoords
         the coordinates of the targets for which a user wants light curves
     labels_list: list of strings
         journal articles associated with the target coordinates
+    plotprint: int
+        print out plots (1) or not(0)
     ztf_radius : float
         search radius, how far from the source should the archives return results
-
+    
     Returns
     -------
     df_lc : MultiIndexDFObject
         the main data structure to store all light curves
     """
+
+    count_nomatch = 0 # counter for non matched objects
+    count_plots = 0 
     df_lc = MultiIndexDFObject()
 
-    # get a df of the info needed to locate each object in the parquet files
-    service = pyvo.dal.TAPService("https://irsa.ipac.caltech.edu/TAP")
-    locations = pd.concat(
-        [locate_object(coord, labels_list, ztf_radius, service) for coord in coords_list],
-        ignore_index=True,
-    )
-
-    # parquet files are organized by filter, field, ccd, and quadrant
-    # group the locations df by file, loop thru files and load, filtering for objectid
-    for key, df in locations.groupby(["filtercode", "field", "ccdid", "qid"]):
-        ztflc_df = pd.read_parquet(
-            file_name(*key),
-            engine="pyarrow",
-            columns=["objectid", "filterid", "nepochs", "hmjd", "mag", "magerr", "catflags"],
-            filters=[("objectid", "in", df["objectid"].to_list())],
-        )
-
-        # add oid and label columns by mapping from object id
-        oidmap = df.set_index("objectid")["oid"].to_dict()
-        lblmap = df.set_index("objectid")["label"].to_dict()
-        ztflc_df["oid"] = ztflc_df["objectid"].map(oidmap)
-        ztflc_df["label"] = ztflc_df["objectid"].map(lblmap)
-
-        # add the band (filtercode) as a column
-        ztflc_df["band"] = key[0]
-
-        # would the "repeated lightcurve" problem be solved by returning only the single closest
-        # object from locate_object()?
-        # if not, choose the one with the largest nepochs?
-
-        # need to convert hmjd -> mjd and label it "time". this isn't right but leaving it for now
-        ztflc_df = ztflc_df.rename(columns={"hmjd": "time"})
-
-        ztflc_df = ztflc_df.explode(["time", "mag", "magerr", "catflags"], ignore_index=True)
-
-        # remove data flagged as bad (catflag = 32768)
-        # guessing we want to remove single datapoints, not full light curves just because one point is bad?
-        ztflc_df = ztflc_df.query("catflags != 32768")
-
-        # calc flux [https://arxiv.org/pdf/1902.01872.pdf zeropoint corrections already applied]
-        magupper = ztflc_df["mag"] + ztflc_df["magerr"]
-        maglower = ztflc_df["mag"] - ztflc_df["magerr"]
-        flux = 10 ** ((ztflc_df["mag"] - 23.9) / -2.5)  # uJy
-        flux_upper = abs(flux - 10 ** ((magupper - 23.9) / -2.5))
-        flux_lower = abs(flux - 10 ** ((maglower - 23.9) / -2.5))
-        fluxerr = (flux_upper + flux_lower) / 2.0
-        ztflc_df.loc[:, "flux"] = flux * 1e-3  # now in mJy
-        ztflc_df.loc[:, "err"] = fluxerr * 1e-3
-
-        df_lc.append(
-            ztflc_df[["flux", "err", "time", "oid", "band", "label"]].set_index(
-                ["oid", "label", "band", "time"]
-            )
-        )
-
+    for objectid, coord in tqdm(coords_list):
+        #doesn't take SkyCoord
+        ra = coord.ra.deg 
+        dec = coord.dec.deg 
+        lab = labels_list[objectid]
+        #make the string for the URL query ask for all three bands (g, r, i)
+        #don't want data that is flagged as unusable by the ZTF pipeline
+        urlstr = 'https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?POS=CIRCLE %f %f %f&BANDNAME=g,r,i&FORMAT=ipac_table&BAD_CATFLAGS_MASK=32768'%(ra, dec,ztf_radius)
+        response = requests.get(urlstr)
+        if response.ok:
+            ztf_lc = ascii.read(response.text, format='ipac')
+            if len(np.unique(ztf_lc['oid']))>0:
+                # reading in indecies of unique IDs to do coordinate match and find dif magnitudes of same objects 
+                idu,inds = np.unique(ztf_lc['oid'],return_index=True)
+                if count_plots<plotprint:
+                    print('object '+str(objectid)+' , unique ztf IDs:'+str(len(np.unique(ztf_lc['oid'])))+',in '+str(len(np.unique(ztf_lc['filtercode'])))+' filters')
+                    plt.figure(figsize=(6,4))
+                
+                ## Neeeded to remove repeated lightcurves in each band. Will keep only the longest following (Sanchez-Saez et al., 2021)
+                filternames = ['zg','zr','zi']
+                filtercounter = [0,0,0] #[g,r,i]
+                filterflux = [[],[],[]]
+            
+                for nn,idd in enumerate(idu):
+                    sel = (ztf_lc['oid']==idd)
+            
+                    flux = 10**((ztf_lc['mag'][sel] - 23.9)/(-2.5))  #now in uJy [Based on ztf paper https://arxiv.org/pdf/1902.01872.pdf zeropoint corrections already applied]
+                    magupper = ztf_lc['mag'][sel] + ztf_lc['magerr'][sel]
+                    maglower = ztf_lc['mag'][sel] - ztf_lc['magerr'][sel]
+                    flux_upper = abs(flux - (10**((magupper - 23.9)/(-2.5))))
+                    flux_lower =  abs(flux - (10**((maglower - 23.9)/(-2.5))))
+                    fluxerr = (flux_upper + flux_lower) / 2.0
+                    flux = flux * (1E-3) # now in mJy
+                    fluxerr = fluxerr * (1E-3) # now in mJy
+                    if count_plots<plotprint:
+                        plt.errorbar(np.round(ztf_lc['mjd'][sel],0),flux,yerr=fluxerr,marker='.',linestyle='',label=ztf_lc['filtercode'][sel][0])
+                        plt.legend()
+                    # if a band is observed before and this has longer lightcurve, remove previous and add this one
+                    if (filtercounter[filternames.index(ztf_lc['filtercode'][sel][0])]>=1) and (len(flux)<=len(filterflux[filternames.index(ztf_lc['filtercode'][sel][0])])):
+                        #print('1st loop, filter'+str(ztf_lc['filtercode'][sel][0])+' len:'+str(len(flux)))
+                        filtercounter[filternames.index(ztf_lc['filtercode'][sel][0])]+=1
+                    elif (filtercounter[filternames.index(ztf_lc['filtercode'][sel][0])]>=1) and (len(flux)>len(filterflux[filternames.index(ztf_lc['filtercode'][sel][0])])):
+                        #print('2nd loop, filter'+str(ztf_lc['filtercode'][sel][0])+' len:'+str(len(flux)))
+                        df_lc.remove((objectid, lab, ztf_lc["filtercode"][sel][0]))
+                        dfsingle = pd.DataFrame(dict(flux=flux, err=fluxerr, time=ztf_lc['mjd'][sel], objectid=objectid, band=ztf_lc['filtercode'][sel][0], label=lab)).set_index(["objectid","label", "band", "time"])
+                        df_lc.append(dfsingle)
+                    else:
+                        #print('3rd filter'+str(ztf_lc['filtercode'][sel][0])+' len:'+str(len(flux)))
+                        dfsingle = pd.DataFrame(dict(flux=flux, err=fluxerr, time=ztf_lc['mjd'][sel], objectid=objectid, band=ztf_lc['filtercode'][sel][0], label=lab)).set_index(["objectid" ,"label","band", "time"])
+                        df_lc.append(dfsingle)
+                        filtercounter[filternames.index(ztf_lc['filtercode'][sel][0])]+=1
+                        filterflux[filternames.index(ztf_lc['filtercode'][sel][0])]=flux
+                    
+                if count_plots<plotprint:
+                    plt.show()
+                    count_plots+=1
+            else:
+                count_nomatch+=1
+    print(count_nomatch,' objects did not match to ztf')
     return df_lc
-
-
-def locate_object(coord, labels_list, ztf_radius, tap_service) -> pd.DataFrame:
-    """Lookup the ZTF field, ccd, and quadrant this coord is located in.
-
-    Parameters
-    ----------
-    coord : tuple
-        the object's ID and astropy skycoords
-    labels_list: list of strings
-        journal articles associated with target coordinates, indexed by object ID
-    ztf_radius : float
-        search radius, how far from the source should the archives return results
-    tap_service : pyvo.dal.TAPService
-        IRSA TAP service
-
-    Returns
-    -------
-    field_df : pd.DataFrame
-        Dataframe with the object's ZTF field, CCD, quadrant and other information useful for
-        locating the object in the parquet files and associating it with this coord.
-    """
-    coord_id, ra, dec = coord[0], coord[1].ra.deg, coord[1].dec.deg
-    # files are organized by filter, field, ccd, and quadrant
-    # look them up using tap. https://irsa.ipac.caltech.edu/docs/program_interface/TAP.html
-    result = tap_service.run_sync(
-        f"SELECT {', '.join(['oid', 'filtercode', 'field', 'ccdid', 'qid', 'ra', 'dec'])} "
-        f"FROM ztf_objects_{DATARELEASE} "
-        "WHERE CONTAINS("
-        # must be one of 'J2000', 'ICRS', and 'GALACTIC'. guessing icrs, but need to check
-        f"POINT('ICRS',ra, dec), CIRCLE('ICRS',{ra},{dec},{ztf_radius.value})"
-        ")=1"
-    )
-    field_df = result.to_table().to_pandas()
-
-    # in the MultiIndexDFObject, "oid" is the name of the coord ID, not the ztf object id
-    field_df = field_df.rename(columns={"oid": "objectid"})
-    field_df["oid"] = coord_id
-    field_df["label"] = labels_list[coord_id]
-
-    # field_df may have more than one oid per band (e.g., yang sample coords_list[10])
-    # guessing we'll want to use the one with the closest ra/dec?
-    # skipping this for now
-
-    return field_df
-
-
-def file_name(filtercode, field, ccdid, qid):
-    """Lookup the filename for this filtercode, field, ccdid, qid.
-
-    Parameters
-    ----------
-    filtercode : str
-        ZTF band name
-    field : int
-        ZTF field
-    ccdid : int
-        ZTF CCD
-    qid : int
-        ZTF quadrant
-
-    Returns
-    -------
-    file_name : str
-        Parquet file name containing this filtercode, field, ccdid, and qid.
-
-    Raises
-    ------
-    AssertionError
-        if exactly one matching file name is not found in the DSFILES list
-    """
-    # can't quite construct the file name directly because need to know whether the top-level
-    # directory is 0 or 1. do a regex search through the DSFILES list.
-    fre = re.compile(f"[01]/field{field:06}/ztf_{field:06}_{filtercode}_c{ccdid:02}_q{qid}")
-    files = [f"{URLBASE}/{f}" for f in filter(fre.match, DSFILES)]
-    # expecting exactly 1 filename. make it fail if there's more or less.
-    assert len(files) == 1, f"found {len(files)} files. expected 1."
-    return files[0]
