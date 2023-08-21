938600fff1fcfcd8c92401b9e766c9fea9cf00f8
Troy Raen <raen@ipac.caltech.edu>
2023-08-21 14:54:17 -0700

    iterate over pixels instead of objects

--- light_curves/code/WISE_functions.py
+++ light_curves/code/WISE_functions.py
@@ -12,6 +12,8 @@ from fluxconversions import convert_wise_flux_to_mag, convert_WISEtoJanskies
 from lsst_formatters import arrow_to_astropy
 
 
+BANDMAP = {1: "w1", 2: "w2"}
+
 #WISE
 def WISE_get_lightcurves(coords_list, labels_list, radius = 1.0 * u.arcsec, bandlist = ['w1', 'w2']):
     """Searches WISE catalog from meisner et al., 2023 for light curves from a list of input coordinates
@@ -39,40 +41,55 @@ def WISE_get_lightcurves(coords_list, labels_list, radius = 1.0 * u.arcsec, band
     dataset = pyarrow.dataset.parquet_dataset(f"{catalog_root}/_metadata", filesystem=fs, partitioning="hive")
     k = 5  # order at which dataset is partitioned
 
-    df_lc = MultiIndexDFObject()
-    for objectid, coord in tqdm(coords_list):
-        lab = labels_list[objectid]
+    # per coord: find pixels
+    # groupby pixels
+    # per pixel group: load file, search for all coords in pixel
+    
+    # locate the WISE partitions/pixels each coords_list object is in
+    my_coords_list = []
+    for objectid, coord in coords_list:
+        cone_pixels = hpgeom.query_circle(
+            a=coord.ra.deg,
+            b=coord.dec.deg,
+            radius=radius.to(u.deg).value,
+            nside=hpgeom.order_to_nside(k),
+            nest=True,  # catalog uses nested ordering scheme for pixel index
+            inclusive=True,  # return all pixels that overlap with the circle, and maybe a few more
+        )
+        my_coords_list.append((objectid, coord, labels_list[objectid], cone_pixels))
+    locations = pd.DataFrame(my_coords_list, columns=["objectid", "coord", "label", "pixel"])
+    # query_circle returned a list of pixels. explode the dataframe into one row per pixel per object
+    locations = locations.explode(["pixel"], ignore_index=True)
 
-        pixels = hpgeom.query_circle(nside=hpgeom.order_to_nside(k), a=coord.ra.deg, b=coord.dec.deg,
-                                     radius=radius.to(u.deg).value, nest=True, inclusive=True)
-        src_tbl = dataset.to_table(columns=["ra", "dec", "band", "flux", "dflux", "MJDMEAN"], 
-                                   filter=(pyarrow.compute.field(f"healpix_k{k}").isin(pixels)))  # pyarrow table
-        src_tbl = arrow_to_astropy(src_tbl)  # astropy table
-        wise_coords = SkyCoord(ra=src_tbl["ra"] * u.degree, dec=src_tbl["dec"] * u.degree)
+    # iterate over partitions/pixels, load data, and find the coords_list objects
+    wise_df_list = []
+    columns = ["flux", "dflux", "ra", "dec", "band", 'MJDMIN', 'MJDMAX', 'MJDMEAN']
+    for pixel, locs_df in locations.groupby("pixel"):
+        objects_skycoords = SkyCoord(locs_df["coord"].to_list())
         
-        result_table = src_tbl[coord.separation(wise_coords) < radius]
-        if (len(result_table) > 0):
-            #got a live one
-            for bcount, band in enumerate(bandlist):
-                #sort by band
-                mask = result_table['band'] == (bcount + 1)
-                result_table_band = result_table[mask]
+        # load all sources (rows) in the partition
+        pixel_tbl = dataset.to_table(filter=(pyarrow.compute.field("healpix_k5") == pixel), columns=columns)
+        pixel_skycoords = SkyCoord(ra=pixel_tbl["ra"] * u.deg, dec=pixel_tbl["dec"] * u.deg)
 
-                mag, magerr = convert_wise_flux_to_mag(result_table_band['flux'], result_table_band['dflux'])
-
-                wiseflux, wisefluxerr = convert_WISEtoJanskies(mag,magerr ,band)
-
-                time_mjd = result_table_band['MJDMEAN']
+        # find sources that are within radius of an object
+        object_ilocs, pixel_ilocs, _, _ = pixel_skycoords.search_around_sky(objects_skycoords, radius)
         
-                #plt.figure(figsize=(8, 4))
-                #plt.errorbar(time_mjd, wiseflux, wisefluxerr)
-                dfsingle = pd.DataFrame(dict(flux=wiseflux, err=wisefluxerr, time=time_mjd, 
+        # create a dataframe with all sources
+        match_df = pixel_tbl.take(pixel_ilocs).to_pandas()
+        # attach the objectid, etc.
+        match_df["object_ilocs"] = object_ilocs
+        match_df = match_df.set_index("object_ilocs").join(locs_df.reset_index(drop=True))
+        
+        wise_df_list.append(match_df)
+    wise_df = pd.concat(wise_df_list, ignore_index=True)
+
+    # transform the data
+    mag, magerr = convert_wise_flux_to_mag(wise_df['flux'], wise_df['dflux'])
+    wiseflux, wisefluxerr = convert_WISEtoJanskies(mag, magerr, wise_df["band"])
+    time_mjd = wise_df['MJDMEAN']
+    band = wise_df["band"].map(BANDMAP)
+    lab = wise_df["label"]
+
+    return MultiIndexDFObject(data=pd.DataFrame(dict(flux=wiseflux, err=wisefluxerr, time=time_mjd, 
                                              objectid=objectid, band=band,label=lab)
-                                       ).set_index(["objectid","label", "band", "time"])
-
-                #then concatenate each individual df together
-                df_lc.append(dfsingle)
-        else:        
-            print("There is no WISE light curve for this object")
-        
-    return(df_lc)
+                                       ).set_index(["objectid","label", "band", "time"]))
