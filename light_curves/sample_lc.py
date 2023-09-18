32ba3d57f37c763f9d9cd618a6fdcc9216746b01
xoubish <shoubaneh@gmail.com>
2023-09-18 19:52:56 +0000

    Fermi sample

--- light_curves/sample_lc.py
+++ light_curves/sample_lc.py
@@ -26,6 +26,7 @@ from heasarc_functions import HEASARC_get_lightcurves
 from TESS_Kepler_functions import TESS_Kepler_get_lightcurves
 from WISE_functions import WISE_get_lightcurves
 from ztf_functions_old import ZTF_get_lightcurve
+from utils import *
 
 
 def build_sample():
@@ -69,6 +70,8 @@ def build_sample():
                  'ZTF20acqoiyt', 'ZTF20abnorit']
     TDE_id2coord(tde_names,coords,labels)
     
+
+    get_paper_sample('2015ApJ...810...14A','FermiBL',coords,labels)
     get_paper_sample('2019A&A...627A..33D','Cicco19',coords,labels)
     get_paper_sample('2022ApJ...933...37W','Galex variable 22',coords,labels)
     get_paper_sample('2020ApJ...896...10B','Palomar variable 20',coords,labels)
@@ -77,79 +80,11 @@ def build_sample():
     coords_list, labels_list = noclean_sample(coords, labels) #!!!!
     print('final sample: ',len(coords))
     return coords_list,labels_list
-
-def parallel_lc(coords_list,labels_list):
-    ''' Check all the archives for the light curve data of the 
-    list of coordinates given in input in parallel and return a 
-    muldidimensional lightcurve dataframe.'''
-    
-    mission_list = ["FERMIGTRIG", "SAXGRBMGRB"]
-    heasarc_radius = 0.1 * u.degree
-    bandlist = ["w1", "w2"]
-    wise_radius = 1.0
-    panstarrs_radius = 1.0 / 3600.0  # search radius = 1 arcsec
-    lk_radius = 1.0  # arcseconds
-    hcv_radius = 1.0 / 3600.0  # radius = 1 arcsec
-    n_single_archives, n_multiple_archives = 6, 2  # must sum to total number of archives called
-    n_chunks_per_archive = 5  # will make one api call per chunk per 'multiple' archive
-    n_workers = n_single_archives + n_multiple_archives * n_chunks_per_archive
-    
-    parallel_starttime = time.time()
-
-    # start a multiprocessing pool and run all the archive queries
-    parallel_df_lc = MultiIndexDFObject()  # to collect the results
-    callback = parallel_df_lc.append  # will be called once on the result returned by each archive
-    with Pool(processes=n_workers) as pool:
-
-        # start the processes that call the fast archives
-        pool.apply_async(
-            Gaia_get_lightcurve, (coords_list, labels_list, 1), callback=callback
-        )
-        pool.apply_async(
-            HEASARC_get_lightcurves, (coords_list, labels_list, heasarc_radius, mission_list), callback=callback
-        )
-        pool.apply_async(
-            HCV_get_lightcurves, (coords_list, labels_list, hcv_radius), callback=callback
-        )
-        pool.apply_async(
-            icecube_get_lightcurve, (coords_list, labels_list, 3, "./data/", 1), callback=callback
-        )
-        pool.apply_async(
-            panstarrs_get_lightcurves, (coords_list, labels_list, panstarrs_radius), callback=callback
-        )
-        pool.apply_async(
-            TESS_Kepler_get_lightcurves, (coords_list, labels_list, lk_radius), callback=callback
-        )
-
-        # split coords_list into smaller chunks and call remaining archives
-        chunksize = ceil(len(coords_list) / n_chunks_per_archive)  # num coords per api call
-        for n in range(0, len(coords_list), chunksize):
-            coords = coords_list[n : n + chunksize]
-
-            # start the processes that call the slow archives
-            pool.apply_async(
-                WISE_get_lightcurves, (coords, labels_list, wise_radius, bandlist), callback=callback
-            )
-            pool.apply_async(
-                ZTF_get_lightcurve, (coords, labels_list, 0), callback=callback
-            )
-
-        pool.close()  # signal that no more jobs will be submitted to the pool
-        pool.join()  # wait for all jobs to complete, including the callback
-
-    parallel_endtime = time.time()
-    print('parallel processing took', parallel_endtime - parallel_starttime, 's')
-    
-    # # Save the data for future use with ML notebook
-    parquet_savename = 'data/df_lc_.parquet.gzip'
-    parallel_df_lc.data.to_parquet(parquet_savename)
-    print("file saved!")
-    return parallel_df_lc
-    
+ 
     
 def main():
     c,l = build_sample()
-    dflc = parallel_lc(c,l)
+    dflc = parallel_lc(c,lparquet_savename = 'data/df_lc_wfermi.parquet.gzip')
     # Unify for ML and save
     
 if __name__ == "__main__":
