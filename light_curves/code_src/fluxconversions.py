bugfix mag_vega formula

light_curves/code_src/fluxconversions.py
@@ -30,7 +30,7 @@ def convert_wise_flux_to_millijansky(flux_nanomaggy, *, band=None):
     vega_to_ab_conv = {"W1": 2.699, "W2": 3.339, "W3": 5.174, "W4": 6.620}
 
     # get Vega magnitude from nanomaggy flux as described in Meisner et al. (2023)
-    mag_vega = 22.5 - np.log10(flux_nanomaggy)
+    mag_vega = 22.5 - 2.5*np.log10(flux_nanomaggy)
 
     # convert Vega magnitude to AB magnitude
     mag_ab = mag_vega + vega_to_ab_conv[band]