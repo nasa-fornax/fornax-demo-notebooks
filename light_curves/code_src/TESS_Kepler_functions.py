636d4c6bdd460ccc13061a3746d4e2c53ea07505
Troy Raen <raen@ipac.caltech.edu>
2024-01-04 11:48:01 -0800

    replace try/except

--- light_curves/code_src/TESS_Kepler_functions.py
+++ light_curves/code_src/TESS_Kepler_functions.py
@@ -57,53 +57,53 @@ def TESS_Kepler_get_lightcurves(sample_table, radius):
     # for testing, this has 79 light curves between the three missions.
     # for ccount in range(1):
     #    coord = '19:02:43.1 +50:14:28.7'
+
+        # use lightkurve to search TESS, Kepler and K2. if nothing is found, continue to the next object.
+        # https://docs.lightkurve.org/tutorials/1-getting-started/searching-for-data-products.html
+        search_result = lk.search_lightcurve(row["coord"], radius = radius)
+        if not search_result:
+            continue
+
+        # download all of the returned light curves from TESS, Kepler, and K2
+        # results occasionally include an unsupported product and this raises a LightkurveError
         try:
-        # use lightkurve to search TESS, Kepler and K2
-            search_result = lk.search_lightcurve(row["coord"], radius = radius)
-            lab = row["label"]
+            lc_collection = search_result.download_all()
+        except lk.LightkurveError:
+            continue
 
-            # figure out what to do with the results
-            if len(search_result) >= 1:
-                # https://docs.lightkurve.org/tutorials/1-getting-started/searching-for-data-products.html
-                print(row["objectid"], 'got a live one')
-                # download all of the returned light curves from TESS, Kepler, and K2
-                lc_collection = search_result.download_all()
-
-                # can't get the whole collection directly into pandas multiindex
-                # pull out inidividual light curves, convert to uniform units, and put them in pandas
-                for numlc in range(len(search_result)):
-            
-                    lc = lc_collection[numlc]  # for testing 0 is Kepler, #69 is TESS
-
-                    # convert to Pandas
-                    lcdf = lc.to_pandas().reset_index()
-        
-                    # these light curves are too highly sampled for our AGN use case, so reduce their size
-                    # by choosing only to keep every nth sample
-                    nsample = 30
-                    lcdf_small = lcdf[lcdf.index % nsample ==0]  # selects every nth row starting with row 0
-            
-                    # convert time to mjd
-                    time_lc = lcdf_small.time # in units of time - 2457000 BTJD days
-                    time_lc= time_lc + 2457000 - 2400000.5 # now in MJD days within a few minutes (except for the barycenter correction)
-
-                    # TESS, Kepler, and K2 report flux in units of electrons/s 
-                    # there is no good way to convert this to anything more useful because the bandpasses are very wide and nonstandard
-                    # really we don't care about absolute scale, but want to scale the light curve to be on the same plot as other light curves
-                    # save as electron/s here and scale when plotting
-                    flux_lc = lcdf_small.flux #in electron/s
-                    fluxerr_lc = lcdf_small.flux_err #in electron/s
-
-                    # record band name
-                    filtername = clean_filternames(search_result, numlc)
-
-                    # put this single object light curves into a pandas multiindex dataframe
-                    # fluxes are in units of electrons/s and will be scaled to fit the other fluxes when plotting
-                    dfsingle = pd.DataFrame(dict(flux=flux_lc, err=fluxerr_lc, time=time_lc, objectid=row["objectid"], band=filtername,label=lab)).set_index(["objectid", "label", "band", "time"])
+        # can't get the whole collection directly into pandas multiindex
+        # pull out inidividual light curves, convert to uniform units, and put them in pandas
+        for numlc in range(len(search_result)):
     
-                    # then concatenate each individual df together
-                    df_lc.append(dfsingle)
-        except:
-            pass
+            lc = lc_collection[numlc]  # for testing 0 is Kepler, #69 is TESS
+
+            # convert to Pandas
+            lcdf = lc.to_pandas().reset_index()
+
+            # these light curves are too highly sampled for our AGN use case, so reduce their size
+            # by choosing only to keep every nth sample
+            nsample = 30
+            lcdf_small = lcdf[lcdf.index % nsample ==0]  # selects every nth row starting with row 0
+    
+            # convert time to mjd
+            time_lc = lcdf_small.time # in units of time - 2457000 BTJD days
+            time_lc= time_lc + 2457000 - 2400000.5 # now in MJD days within a few minutes (except for the barycenter correction)
+
+            # TESS, Kepler, and K2 report flux in units of electrons/s 
+            # there is no good way to convert this to anything more useful because the bandpasses are very wide and nonstandard
+            # really we don't care about absolute scale, but want to scale the light curve to be on the same plot as other light curves
+            # save as electron/s here and scale when plotting
+            flux_lc = lcdf_small.flux #in electron/s
+            fluxerr_lc = lcdf_small.flux_err #in electron/s
+
+            # record band name
+            filtername = clean_filternames(search_result, numlc)
+
+            # put this single object light curves into a pandas multiindex dataframe
+            # fluxes are in units of electrons/s and will be scaled to fit the other fluxes when plotting
+            dfsingle = pd.DataFrame(dict(flux=flux_lc, err=fluxerr_lc, time=time_lc, objectid=row["objectid"], band=filtername,label=row["label"])).set_index(["objectid", "label", "band", "time"])
+
+            # then concatenate each individual df together
+            df_lc.append(dfsingle)
         
     return(df_lc)
