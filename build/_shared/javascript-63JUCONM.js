76b935c85eff14ee9526c2acbc6a4ee1bfe9999f
bsipocz <bsipocz@users.noreply.github.com>
2025-12-20 02:33:32 +0000

    Merge pull request #595 from bsipocz/MAINT_landing_slugs
    MAINT: adding sensible slugs for the landing pages 49dfdb98bade5aac995646aee5b64d04579c72e3

---
+++ build/_shared/javascript-63JUCONM.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"/fornax-demo-notebooks/build/_shared/chunk-265RPIE7.js";import"/fornax-demo-notebooks/build/_shared/chunk-OZE3FFNP.js";e();export{a as javascript,b as json,c as jsonld,d as typescript};
