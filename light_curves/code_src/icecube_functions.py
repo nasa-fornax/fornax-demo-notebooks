afb6fc743db39e2c3a5b7cca80bca8e749b7fbd4
Troy Raen <raen@ipac.caltech.edu>
2026-07-24 18:39:32 -0700

    Switch to TAP query for IceCube data

--- light_curves/code_src/icecube_functions.py
+++ light_curves/code_src/icecube_functions.py
@@ -1,20 +1,9 @@
 # Functions related to IceCube matching
-import os
-import zipfile
-from urllib.parse import urlparse
-from urllib.request import urlretrieve
-
-import astropy.units as u
 import numpy as np
 import pandas as pd
-from astropy.coordinates import SkyCoord
-from astropy.io import ascii
-from astropy.table import Table, vstack
+from astroquery.heasarc import Heasarc
 
 from data_structures import MultiIndexDFObject
-
-DATA_PATH = os.path.dirname(os.path.dirname(__file__)) + \
-    "/data/"  # absolute path to light_curves/data/
 
 
 def icecube_get_lightcurves(sample_table, *, icecube_select_topN=3, max_search_radius=2.0):
@@ -47,7 +36,7 @@ def icecube_get_lightcurves(sample_table, *, icecube_select_topN=3, max_search_r
      df_lc : MultiIndexDFObject
         Indexed by [objectid, label, band, time]. The resulting internal pandas DataFrame
         contains the following columns:
-        
+
             flux : float
                 Neutrino event energy expressed as log10(GeV).
             err : float
@@ -61,46 +50,14 @@ def icecube_get_lightcurves(sample_table, *, icecube_select_topN=3, max_search_r
             label : str
                 Literature label associated with each source.
     '''
-    max_search_radius = max_search_radius * u.deg
-
-    # Downloads the IceCube data and unzipps it. Only does it if the files have not yet been downloaded. The
-    # total file size is about 30MB (zipped) and 110MB unzipped.
-    icecube_download_data(verbose=False)
-
-    # This loads the IceCube catalog, which is dispersed in different files.
-    # Each file has a list of events with their energy, time, and approximate direction.
-    icecube_events, _ = icecube_get_catalog(verbose=False)
-
-    # create SkyCoord objects from icecube event coordinates
-    icecube_skycoords = SkyCoord(
-        icecube_events["ra"], icecube_events["dec"], unit="deg", frame='icrs')
-
-    # here are the skycoords from mysample defined above
-    mysample_skycoords = sample_table['coord']
-
-    # Match
-    idx_mysample, idx_icecube, d2d, d3d = icecube_skycoords.search_around_sky(
-        mysample_skycoords, max_search_radius)
-
-    # need to filter reponse based on position error circles
-    # really want d2d to be less than the error circle of icecube = icecube_events["AngErr"] in degrees
-    filter_arr = d2d < icecube_events["AngErr"][idx_icecube]
-    filter_idx_mysample = idx_mysample[filter_arr]
-    filter_idx_icecube = idx_icecube[filter_arr]
-    filter_d2d = d2d[filter_arr]
-
-    # keep these matches together with objectid and lebal as new entries in the df.
-    obid_match = sample_table['objectid'][filter_idx_mysample]
-    label_match = sample_table['label'][filter_idx_mysample]
-    time_icecube = icecube_events['mjd'][filter_idx_icecube]
-    flux_icecube = icecube_events['energy_logGeV'][filter_idx_icecube]
+    icecube_tbl = icecube_query_catalog(sample_table, max_search_radius=max_search_radius)
 
     # save the icecube info in correct format for the rest of the data
-    icecube_df = pd.DataFrame({'flux': flux_icecube,
-                               'err': np.zeros(len(obid_match)),
-                               'time': time_icecube,
-                               'objectid': obid_match,
-                               'label': label_match,
+    icecube_df = pd.DataFrame({'flux': icecube_tbl['cat_event_energy'],
+                               'err': np.zeros(len(icecube_tbl)),
+                               'time': icecube_tbl['cat_time'],
+                               'objectid': icecube_tbl['samp_objectid'],
+                               'label': icecube_tbl['samp_label'],
                                'band': "IceCube"})
 
     # sort by Neutrino energy that way it is easier to get the top N events.
@@ -116,111 +73,53 @@ def icecube_get_lightcurves(sample_table, *, icecube_select_topN=3, max_search_r
     return (MultiIndexDFObject(data=filter_icecube_df))
 
 
-def icecube_get_catalog(path=DATA_PATH, verbose=False):
+def icecube_query_catalog(sample_table, *, max_search_radius):
     '''
-    Creates the combined IceCube catalog based on the yearly catalogs.
-
-    Parameters
-    -----------
-    path : str
-        Download directory path
-    verbose : bool
-        Default False. Display extra info and warnings if true.
-
-    Returns
-    --------
-    EVENTS : astropy.table.Table
-        Combined event table with columns:
-            mjd : float
-                Event detection time (Modified Julian Date).
-            energy_logGeV : float
-                log10(Energy / GeV).
-            AngErr : float
-                Angular positional uncertainty in degrees.
-            ra : float
-                Right ascension of event direction (deg).
-            dec : float
-                Declination of event direction (deg).
-            az : float
-                Azimuth (deg).
-            zen : float
-                Zenith angle (deg).
-
-    event_names : list of str
-        Filenames of the event catalog files that were read and combined.
-    '''
-
-    event_names = ["IC40_exp.csv",
-                   "IC59_exp.csv",
-                   "IC79_exp.csv",
-                   "IC86_III_exp.csv",
-                   "IC86_II_exp.csv",
-                   "IC86_IV_exp.csv",
-                   "IC86_I_exp.csv",
-                   "IC86_VII_exp.csv",
-                   "IC86_VI_exp.csv",
-                   "IC86_V_exp.csv"
-                   ]
-
-    EVENTS = Table(names=["mjd", "energy_logGeV", "AngErr", "ra", "dec", "az", "zen"],
-                   units=[u.d, u.GeV, u.degree, u.degree, u.degree, u.degree, u.degree])
-
-    for event_name in event_names:
-        if verbose:
-            print("Loading: ", event_name)
-        tmp = ascii.read(os.path.join(path, "icecube_10year_ps", "events", event_name))
-        tmp.rename_columns(names=tmp.keys(), new_names=EVENTS.keys())
-
-        EVENTS = vstack([EVENTS, tmp])
-    if verbose:
-        print("done")
-    return (EVENTS, event_names)
-
-
-def icecube_download_data(url="http://icecube.wisc.edu/data-releases/20210126_PS-IC40-IC86_VII.zip",
-                          path=DATA_PATH, verbose=False):
-    '''
-    Download and unzipps the IceCube data (approx. 40MB zipped, 120MB unzipped). Directly
-    downloaded from the IceCube webpage:
-    https://icecube.wisc.edu/data-releases/2021/01/all-sky-point-source-icecube-data-years-2008-2018/
+    Cross match the input sample against the IceCube All-Sky Point-Source Neutrino Events Catalog (2008-2018)
+    using a TAP query with a table upload via `astroquery.heasarc.Heasarc`.
 
     Parameters
     ----------
-    url : str
-        Icecube data URL
-    path : str
-        Destination directory for downloaded and unzipped data.
-    verbose : bool
-        Default False. Display extra info and warnings if true.
+    sample_table : astropy.table.Table
+        Table containing the source sample. The following columns must be present:
+            coord : astropy.coordinates.SkyCoord
+                Sky position of each source.
+            objectid : int
+                Unique identifier for each source in the sample.
+            label : str
+                Literature label for tracking source provenance.
+    max_search_radius : float
+        Maximum radius (degrees) to look for matches in IceCube. Actual match radius will not exceed the
+        IceCube error of an individual event. Beware that setting this to a high number can cause
+        the code to look through a large number of potential matches for each object, which may
+        impact performance.
 
     Returns
     -------
-    None
-        Extracted data files are written to disk in `path`.
-
+    astropy.table.Table
+        One row per matched IceCube event, with columns `samp_objectid`, `samp_label`, `cat_time` [MJD],
+        and `cat_event_energy` [log10(GeV)].
     '''
+    # Put sample_table into the format TAP expects.
+    upload_table = sample_table['objectid', 'label']
+    upload_table['ra'] = sample_table['coord'].ra.deg
+    upload_table['dec'] = sample_table['coord'].dec.deg
 
-    file_path = os.path.join(path, os.path.basename(urlparse(url).path))
+    # ADQL to find IceCube events within max_search_radius of our sample targets.
+    # AND DISTANCE removes events with an error radius less than the distance between the event and our sample target.
+    icecube_catalog = "icecubepsc"
+    query = f"""
+        SELECT samp.objectid, samp.label, cat.time, cat.event_energy
+        FROM {icecube_catalog} cat, tap_upload.sample_table samp
+        WHERE 1=CONTAINS(POINT('ICRS', samp.ra, samp.dec), CIRCLE('ICRS', cat.ra, cat.dec, {max_search_radius}))
+        AND DISTANCE(POINT('ICRS', samp.ra, samp.dec), POINT('ICRS', cat.ra, cat.dec)) < cat.error_radius
+    """
 
-    # if the data has not already been downloaded
-    if not os.path.exists(file_path):
+    # Query HEASARC for IceCube events within our sample target areas.
+    # This will prepend the catalog name ("samp" or "cat") to the column names, eg, "time" -> "cat_time".
+    icecube_tbl = Heasarc.query_tap(query, uploads={"sample_table": upload_table}).to_table()
 
-        # Download
-        if verbose:
-            print(f"Downloading IceCube data to {file_path}.")
+    # We want log10 of the event energy.
+    icecube_tbl['cat_event_energy'] = np.log10(icecube_tbl['cat_event_energy'])
 
-        _ = urlretrieve(url, file_path)
-
-        # Unzip
-        if verbose:
-            print("Unzipping IceCube data.")
-
-        with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(path)
-
-        if verbose:
-            print("Done.")
-
-    else:
-        if verbose:
-            print(f"Data is already downloaded, see {path}")
+    return icecube_tbl
